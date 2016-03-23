7edba15c59210b12719e0af92c4c8835795f2e00
Yicheng Guo <yicheng.guo@cern.ch>
2016-03-23 18:27:40 +0100

    add header

--- www/app.py
+++ www/app.py
@@ -1,5 +1,9 @@
-import logging; logging.basicConfig(level=logging.INFO)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+__author__ = 'Yicheng Guo'
+
+import logging; logging.basicConfig(level=logging.INFO)
 import asyncio, os, json, time
 from datetime import datetime
 from aiohttp import web
