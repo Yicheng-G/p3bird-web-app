ff5c24272bbb8a3a89f0e658d9815e2fb461e573
Yicheng Guo <ycguo@cern.ch>
2016-03-31 22:35:28 +0200

    add configurations

---
+++ www/config_default.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Default configurations.
+"""
+
+__author__ = 'Yicheng Guo'
+
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'user': 'ycguo',
+        'password': '',
+        'db': 'p3bird',
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Override configurations.
+"""
+
+__author__ = 'Yicheng Guo'
+
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+    }
+}
